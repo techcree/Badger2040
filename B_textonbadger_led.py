fd49d200710845a2503d21c36610c60075154994
Stephan Skanta <78694630+techcree@users.noreply.github.com>
2023-01-29 16:46:29 +0100

    Create B_textonbadger_led.py
    Alternate with reading variables from varfiles

---
+++ B_textonbadger_led.py
@@ -0,0 +1,32 @@
+#load libaries 
+import badger2040
+import led
+from inttext import settext, setroom, setcompany
+import setroom_file, settext_file, setcompany_file
+
+#load badger
+badger = badger2040.Badger2040()
+
+#set fontstyles
+COMPANY_HEIGHT = 30
+DETAILS_HEIGHT = 20
+COMPANY_TEXT_SIZE = 0.6
+HEIGHT_TEXT_SIZE = 0.5
+LEFT_PADDING = 5
+NAME_PADDING = 20
+DETAIL_SPACING = 10
+OVERLAY_BORDER = 40
+OVERLAY_SPACING = 20
+OVERLAY_TEXT_SIZE = 0.6
+
+#set badger
+badger.pen(0)
+
+#print text on badger
+badger.thickness(3)
+badger.text((setroom_file.setroom), 20, 20, 1)
+badger.thickness(2)
+badger.text((settext_file.settext), 20, 70, 0.8)
+badger.thickness(1)
+badger.text((setcompany_file.setcompany), 20, 100, 0.6)
+badger.update()
