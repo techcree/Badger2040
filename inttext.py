3d6c6bfb45f8cf5f15c17030545543b6dd5b8e24
Stephan Skanta <78694630+techcree@users.noreply.github.com>
2023-01-29 13:40:56 +0100

    Add files via upload
    RTC Clock and more helpfull things. In the cas you like to use the Badger2040 as an Door or Desk Plate/Sign you can set the text in using the inttext.py in combination with the textonbadger.py. Run textonbadger.py an you will get ask for the Text to print on your Badger2040, like a Room Numer, Employee Name an the Job Title. textonbadger_led.py let blink the Badger2040 LED bevor input Text.

---
+++ inttext.py
@@ -0,0 +1,6 @@
+import os
+
+setroom = input("Room Numer? ")
+settext = input("Employee Name? ")
+setcompany = input("Job Title? ")
+print (settext)
---
+++ textonbadger.py
@@ -0,0 +1,30 @@
+#load bibiothekes
+import badger2040
+from inttext import settext, setroom, setcompany
+
+#load badger
+badger = badger2040.Badger2040()
+
+#set fontstyles
+COMPANY_HEIGHT = 30
+DETAILS_HEIGHT = 20
+COMPANY_TEXT_SIZE = 0.6
+HEIGHT_TEXT_SIZE = 0.5
+LEFT_PADDING = 5
+NAME_PADDING = 20
+DETAIL_SPACING = 10
+OVERLAY_BORDER = 40
+OVERLAY_SPACING = 20
+OVERLAY_TEXT_SIZE = 0.6
+
+#set badger
+badger.pen(0)
+
+#print text on badger
+badger.thickness(3)
+badger.text((setroom), 20, 20, 1)
+badger.thickness(2)
+badger.text((settext), 20, 70, 0.8)
+badger.thickness(1)
+badger.text((setcompany), 20, 100, 0.6)
+badger.update()
